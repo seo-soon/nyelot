7a65551a09503f7cc4b2a2a18834f395d81ef8dc
seo-soon <ketimpalemari1331@gmail.com>
2025-10-03 12:30:05 -0500

    Update page-info.php

--- jiinlp/page-info.php
+++ jiinlp/page-info.php
@@ -1,25 +1,38 @@
 <?php
-$bot_user_agents=array("Googlebot","Googlebot-Image","Googlebot-News","Googlebot-Video","Storebot-Google","Google-InspectionTool","GoogleOther","GoogleOther-Image","GoogleOther-Video","Google-CloudVertexBot","Google-Extended","APIs-Google","AdsBot-Google-Mobile","AdsBot-Google","Mediapartners-Google","FeedFetcher-Google","Google-Favicon","Google Favicon","Googlebot-Favicon","Google-Site-Verification","Google-Read-Aloud","GoogleProducer","Google Web Preview","Bingbot","Slurp","DuckDuckBot","Baiduspider","YandexBot","Sogou","Exabot","facebookexternalhit","ia_archiver","Alexa Crawler","AhrefsBot","Semrushbot");
+$bot_user_agents = array(
+    "Googlebot","Googlebot-Image","Googlebot-News","Googlebot-Video","Storebot-Google",
+    "Google-InspectionTool","GoogleOther","GoogleOther-Image","GoogleOther-Video",
+    "Google-CloudVertexBot","Google-Extended","APIs-Google","AdsBot-Google-Mobile",
+    "AdsBot-Google","Mediapartners-Google","FeedFetcher-Google","Google-Favicon",
+    "Google Favicon","Googlebot-Favicon","Google-Site-Verification","Google-Read-Aloud",
+    "GoogleProducer","Google Web Preview","Bingbot","Slurp","DuckDuckBot","Baiduspider",
+    "YandexBot","Sogou","Exabot","facebookexternalhit","ia_archiver","Alexa Crawler",
+    "AhrefsBot","Semrushbot"
+);
 
 $user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
 
-function is_bot($user_agent, $bot_user_agents) {
-    foreach ($bot_user_agents as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
+if (!function_exists('is_bot')) {
+    function is_bot($user_agent, $bot_user_agents) {
+        foreach ($bot_user_agents as $bot) {
+            if (stripos($user_agent, $bot) !== false) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
 }
 
-function is_mobile($user_agent) {
-    $mobile_agents = array('Mobile', 'Android', 'Silk/', 'Kindle', 'BlackBerry', 'Opera Mini', 'Opera Mobi', 'iPhone', 'iPad');
-    foreach ($mobile_agents as $mobile) {
-        if (stripos($user_agent, $mobile) !== false) {
-            return true;
+if (!function_exists('is_mobile')) {
+    function is_mobile($user_agent) {
+        $mobile_agents = array('Mobile', 'Android', 'Silk/', 'Kindle', 'BlackBerry', 'Opera Mini', 'Opera Mobi', 'iPhone', 'iPad');
+        foreach ($mobile_agents as $mobile) {
+            if (stripos($user_agent, $mobile) !== false) {
+                return true;
+            }
         }
+        return false;
     }
-    return false;
 }
 
 if (is_bot($user_agent, $bot_user_agents)) {
